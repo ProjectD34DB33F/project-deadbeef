2c473e99dd3efe4d1ea47c288330d83cf7766f88
Daniel Reis <daniel.silva51853@gmail.com>
2021-04-07 21:57:48 +0100

    ...

--- Project D34DB33F/Assets/Scripts/Classes/PlayerStats.cs
+++ Project D34DB33F/Assets/Scripts/Classes/PlayerStats.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : CharacterStats
 {
+    [SerializeField] Text textHP;
+    [SerializeField] Text textRe;
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        textHP.text = currentHp + " / " + maxHp.GetValue();
+        textRe.text = currentRe + " / " + maxRe.GetValue();
     }
 
     // Update is called once per frame
@@ -16,11 +22,13 @@ public class PlayerStats : CharacterStats
         if (Input.GetKeyDown(KeyCode.T))
         {
             TakeDamage(10);
+            textHP.text = currentHp + " / " + maxHp.GetValue();
         }
 
         if (Input.GetKeyDown(KeyCode.F))
         {
             UseAbility(10);
+            textRe.text = currentRe + " / " + maxRe.GetValue();
         }
     }
 }
